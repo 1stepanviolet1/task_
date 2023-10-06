f6f542912e52d4fac1875cab041127a9bb93eb25
1stepanviolet1 <stepanviolet@gmail.com>
2023-10-06 04:45:14 +0300

    --

--- index_first_negative.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-#include <stdio.h>
-
-
-int index_first_negative(int[], size_t);
-
--- index_last_negative.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-
-#include <stdio.h>
-
-int index_last_negative(int[], size_t);
-
