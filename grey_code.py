af835937c11cdfea7c617b7590d8d706c7f2e31c
1stepanviolet1 <stepanviolet@gmail.com>
2024-10-29 16:18:04 +0300

    grey code

---
+++ grey_code.py
@@ -0,0 +1,39 @@
+
+
+def non_cyclic_right_shift(number: int, shift: int = 1) -> int:
+    if number < 0:
+        raise ValueError("Число должно быть неотрицательным")
+    
+    shifted_number = number >> shift
+    mask = (1 << (number.bit_length() - shift)) - 1
+    
+    return shifted_number & mask
+
+
+def grey_code(x: int) -> int:
+    return x ^ non_cyclic_right_shift(x)
+
+
+def reverce_grey_code(x: int) -> int:
+    if x < 0:
+        raise ValueError("Число должно быть неотрицательным")
+    
+    num_of_bit = x.bit_length() - 1
+    n = (x >> num_of_bit) & 1
+    
+    prev_bit = n
+    while num_of_bit:
+        num_of_bit -= 1
+        bit = (x >> num_of_bit) & 1
+        g_bit = bit ^ prev_bit
+        n *= 0b10
+        n += g_bit
+        prev_bit = g_bit
+
+    return n
+
+
+if __name__ == '__main__':
+    print(reverce_grey_code(30))
+    # print()
+
